7456b3c80c42dfd21b006b70d12c463f22095a62
Shubh Bapna <sbapna@redhat.com>
2022-11-02 15:27:58 -0400

    added getOrderedListForProject

--- src/tree.ts
+++ src/tree.ts
@@ -38,11 +38,31 @@ export function parentChainFromNode(node: Node): Node[] {
   return result;
 }
 
+export function getOrderedListForProject(node: Node) {
+  const upstream: Node[] = parentChainFromNode(node); // last element is the current node
+  const downstream: Node[] = childChainFromNode(node); // first element is the current node
+  return upstream.concat(downstream.slice(1)); // avoid duplication of current node
+}
+
 function lookForProject(tree: Node[], project: string): Node | undefined {
   return (
     tree.find(node => node.project === project) ??
     tree
       .map(node => lookForProject(node.children, project))
       .find(node => !!node)
+  );
+}
+
+function childChainFromNode(node: Node): Node[] {
+  return node.children.reduce(
+    (children: Node[], child: Node) => {
+      children.push(
+        ...childChainFromNode(child).filter(
+          c => !children.find(e => e.project === c.project)
+        )
+      );
+      return children;
+    },
+    [node]
   );
 }
