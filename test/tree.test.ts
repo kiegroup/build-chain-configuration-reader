15bd52e80a5f187103ee9688f63b09225d13e235
Shubh Bapna <sbapna@redhat.com>
2022-11-02 15:28:15 -0400

    refactored tree test to use real definition file. added tests for getOrderedLostForProject

--- test/tree.test.ts
+++ test/tree.test.ts
@@ -1,118 +1,152 @@
 import { Node } from "@bc-cr/domain/node";
-import * as reader from "@bc-cr/reader";
-import { getTree, getTreeForProject, parentChainFromNode } from "@bc-cr/tree";
-import * as constructTree from "@bc-cr/util/construct-tree";
+import {
+  getOrderedListForProject,
+  getTree,
+  getTreeForProject,
+  parentChainFromNode,
+} from "@bc-cr/tree";
+import path from "path";
+const resources = path.join(__dirname, "resources", "tree", "tree.yml");
 
 test("get tree", async () => {
-  const readDefinitionFileSpy = jest
-    .spyOn(reader, "readDefinitionFile")
-    .mockImplementation(async () => ({ dependencies: [], version: "2.1" }));
-  const constructTreeSpy = jest
-    .spyOn(constructTree, "constructTree")
-    .mockImplementation(() => []);
-  await expect(getTree("definitionFile")).resolves.toStrictEqual([]);
-  expect(readDefinitionFileSpy).toHaveBeenCalledTimes(1);
-  expect(constructTreeSpy).toHaveBeenCalledTimes(1);
+  const tree = await getTree(resources);
+  // don't have to test for tree structure since that is already done in construct tree tests
+  matchProject(tree, [
+    "kiegroup/lienzo-core",
+    "kiegroup/droolsjbpm-build-bootstrap",
+    "kiegroup/kie-docs",
+  ]);
 });
 
-const nodeChain: Node[] = [
-  {
-    project: "project1",
-    children: [
-      {
-        project: "project4",
-        children: [
-          {
-            project: "project6",
-            children: [],
-            parents: [
-              {
-                project: "project4",
-                children: [],
-                parents: [{ project: "project1", children: [], parents: [] }],
-              },
-            ],
-          },
-        ],
-        parents: [{ project: "project1", children: [], parents: [] }],
-      },
-      {
-        project: "project7",
-        children: [],
-        parents: [{ project: "project1", children: [], parents: [] }],
-      },
-    ],
-    parents: [],
-  },
-  { project: "project2", children: [], parents: [] },
-  {
-    project: "project3",
-    children: [
-      {
-        project: "project5",
-        children: [],
-        parents: [{ project: "project3", parents: [], children: [] }],
-      },
-    ],
-    parents: [],
-  },
-];
+test.each([
+  ["root", "kiegroup/kie-docs"],
+  ["non-root", "kiegroup/jbpm"],
+  ["not found", "kiegroup/unknownproject"],
+])("get tree for project - %p", async (_title: string, project: string) => {
+  const node = await getTreeForProject(resources, project);
+  matchProject(node ? [node] : node, [project]);
+});
 
 test.each([
-  ["root", "project2", nodeChain[1]],
-  ["non-root", "project6", nodeChain[0].children[0].children[0]],
-  ["not found", "project10", undefined],
-])(
-  "get tree for project - %p",
-  async (_title: string, project: string, expected: Node | undefined) => {
-    jest
-      .spyOn(reader, "readDefinitionFile")
-      .mockImplementation(async () => ({ dependencies: [], version: "2.1" }));
-    jest
-      .spyOn(constructTree, "constructTree")
-      .mockImplementation(() => nodeChain);
-
-    await expect(
-      getTreeForProject("definitionFile", project)
-    ).resolves.toStrictEqual(expected);
-  }
-);
-
-test.each([
-  ["no parents", nodeChain[1], [nodeChain[1]]],
+  [
+    "no parents",
+    "kiegroup/droolsjbpm-build-bootstrap",
+    ["kiegroup/droolsjbpm-build-bootstrap"],
+  ],
   [
     "parents",
-    nodeChain[0].children[0].children[0],
+    "kiegroup/appformer",
     [
-      { project: "project1", children: [], parents: [] },
-      {
-        project: "project4",
-        children: [],
-        parents: [{ project: "project1", children: [], parents: [] }],
-      },
-      {
-        project: "project6",
-        children: [],
-        parents: [
-          {
-            project: "project4",
-            children: [],
-            parents: [{ project: "project1", children: [], parents: [] }],
-          },
-        ],
-      },
+      "kiegroup/lienzo-core",
+      "kiegroup/lienzo-tests",
+      "kiegroup/droolsjbpm-build-bootstrap",
+      "kiegroup/kie-soup",
+      "kiegroup/appformer",
     ],
   ],
 ])(
   "get parent chain for node - %p",
-  (_title: string, project: Node, expected: Node[] | undefined) => {
-    jest
-      .spyOn(reader, "readDefinitionFile")
-      .mockImplementation(async () => ({ dependencies: [], version: "2.1" }));
-    jest
-      .spyOn(constructTree, "constructTree")
-      .mockImplementation(() => nodeChain);
-
-    expect(parentChainFromNode(project)).toStrictEqual(expected);
+  async (_title: string, project: string, expected: string[]) => {
+    const node = await getTreeForProject(resources, project);
+    expect(node).not.toBe(undefined);
+    const parents = parentChainFromNode(node!);
+    matchProject(parents, expected);
   }
 );
+
+test.each([
+  [
+    "only upstream",
+    "kiegroup/kie-wb-distributions",
+    [
+      "kiegroup/lienzo-core",
+      "kiegroup/lienzo-tests",
+      "kiegroup/droolsjbpm-build-bootstrap",
+      "kiegroup/kie-soup",
+      "kiegroup/appformer",
+      "kiegroup/kie-uberfire-extensions",
+      "kiegroup/droolsjbpm-knowledge",
+      "kiegroup/drools",
+      "kiegroup/jbpm",
+      "kiegroup/kie-jpmml-integration",
+      "kiegroup/optaplanner",
+      "kiegroup/droolsjbpm-integration",
+      "kiegroup/kie-wb-playground",
+      "kiegroup/kie-wb-common",
+      "kiegroup/drools-wb",
+      "kiegroup/optaplanner-wb",
+      "kiegroup/jbpm-designer",
+      "kiegroup/jbpm-work-items",
+      "kiegroup/jbpm-wb",
+      "kiegroup/kie-wb-distributions",
+    ],
+  ],
+  [
+    "only downstream",
+    "kiegroup/droolsjbpm-build-bootstrap",
+    [
+      "kiegroup/droolsjbpm-build-bootstrap",
+      "kiegroup/kie-soup",
+      "kiegroup/appformer",
+      "kiegroup/kie-uberfire-extensions",
+      "kiegroup/kie-wb-common",
+      "kiegroup/drools-wb",
+      "kiegroup/optaplanner-wb",
+      "kiegroup/kie-wb-distributions",
+      "kiegroup/jbpm-wb",
+      "kiegroup/jbpm-designer",
+      "kiegroup/droolsjbpm-knowledge",
+      "kiegroup/drools",
+      "kiegroup/jbpm",
+      "kiegroup/kie-jpmml-integration",
+      "kiegroup/droolsjbpm-integration",
+      "kiegroup/openshift-drools-hacep",
+      "kiegroup/kie-wb-playground",
+      "kiegroup/jbpm-work-items",
+      "kiegroup/optaplanner",
+      "kiegroup/optaweb-employee-rostering",
+      "kiegroup/optaweb-vehicle-routing",
+    ],
+  ],
+  [
+    "upstream + downstream",
+    "kiegroup/jbpm",
+    [
+      "kiegroup/droolsjbpm-build-bootstrap",
+      "kiegroup/kie-soup",
+      "kiegroup/droolsjbpm-knowledge",
+      "kiegroup/drools",
+      "kiegroup/jbpm",
+      "kiegroup/kie-jpmml-integration",
+      "kiegroup/droolsjbpm-integration",
+      "kiegroup/openshift-drools-hacep",
+      "kiegroup/kie-wb-playground",
+      "kiegroup/kie-wb-common",
+      "kiegroup/drools-wb",
+      "kiegroup/optaplanner-wb",
+      "kiegroup/kie-wb-distributions",
+      "kiegroup/jbpm-wb",
+      "kiegroup/jbpm-designer",
+      "kiegroup/jbpm-work-items",
+    ],
+  ],
+])(
+  "get ordered list for project - %p",
+  async (_title: string, project: string, expected: string[]) => {
+    const node = await getTreeForProject(resources, project);
+    expect(node).not.toBe(undefined);
+    const list = getOrderedListForProject(node!);
+    matchProject(list, expected);
+  }
+);
+
+function matchProject(
+  result: Node[] | undefined,
+  expected: string[] | undefined
+) {
+  if (result) {
+    expect(result.map(node => node.project)).toStrictEqual(expected);
+  } else {
+    expect(result).toBe(undefined);
+  }
+}
