d0788fc22c7ab5b7007a582a2ec925735eaf495f
Shubh Bapna <sbapna@redhat.com>
2022-11-14 11:27:46 -0500

    updated definition file domain object

--- src/domain/definition-file.ts
+++ src/domain/definition-file.ts
@@ -2,11 +2,13 @@ import { Build, BuildCommand } from "@bc-cr/domain/build";
 import { Dependency } from "@bc-cr/domain/dependencies";
 
 export interface DefinitionFile {
-  version: "2.1" | 2.1
-  dependencies: string | Dependency[]
+  version: "2.1" | 2.1 | "2.2" | 2.2;
+  dependencies?: string | Dependency[];
   extends?: string;
   default?: {
-    "build-command": BuildCommand
-  }
-  build?: Build[]
+    "build-command": BuildCommand;
+  };
+  build?: Build[];
+  pre?: string[];
+  post?: string[];
 }
