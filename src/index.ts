8429c553399862d60ac01b95d5884dd24432089a
Shubh Bapna <sbapna@redhat.com>
2022-11-15 09:46:04 -0500

    added tsc-alias and types in build

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import {
+  getTree,
+  parentChainFromNode,
+  getOrderedListForProject,
+  getOrderedListForTree,
+  getTreeForProject,
+} from "@bc-cr/tree";
+
+import { readDefinitionFile } from "@bc-cr/reader";
+
+import { getMappedTarget, getMapping } from "@bc-cr/util/mapping";
+
+export {
+  getTree,
+  parentChainFromNode,
+  getOrderedListForProject,
+  getOrderedListForTree,
+  getTreeForProject,
+  readDefinitionFile,
+  getMappedTarget,
+  getMapping
+};
