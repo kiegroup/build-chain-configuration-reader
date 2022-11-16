9fd32c4b1e583fedcaffdd1fce65761c607f00ee
Shubh Bapna <sbapna@redhat.com>
2022-11-16 08:46:59 -0500

    added types

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,14 @@ import { readDefinitionFile } from "@bc-cr/reader";
 
 import { getMappedTarget, getMapping } from "@bc-cr/util/mapping";
 
+import { ArchiveArtifacts } from "@bc-cr/domain/archive-artifacts";
+import { Command, CommandLevel, BuildCommand, Build } from "@bc-cr/domain/build";
+import { DefinitionFile } from "@bc-cr/domain/definition-file";
+import { Dependency } from "@bc-cr/domain/dependencies";
+import { Mapping } from "@bc-cr/domain/mapping";
+import { Node } from "@bc-cr/domain/node";
+import { ReaderOpts } from "@bc-cr/domain/readerOptions";
+
 export {
   getTree,
   parentChainFromNode,
@@ -18,5 +26,12 @@ export {
   getTreeForProject,
   readDefinitionFile,
   getMappedTarget,
-  getMapping
+  getMapping,
+  ArchiveArtifacts,
+  Command, CommandLevel, BuildCommand, Build,
+  DefinitionFile,
+  Dependency,
+  Mapping,
+  Node,
+  ReaderOpts
 };
