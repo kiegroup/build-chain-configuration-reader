d6a8473eb2adbf4535ecb53dcea1afa37387ea77
Shubh Bapna <sbapna@redhat.com>
2022-11-20 21:46:58 -0500

    add enums that are used by build chain

--- src/domain/archive-artifacts.ts
+++ src/domain/archive-artifacts.ts
@@ -1,9 +1,25 @@
 export interface ArchiveArtifacts {
   name?: string;
-  "if-no-files-found": "warn" | "error" | "ignore";
-  dependencies: string[] | "all" | "none";
+  "if-no-files-found": `${IfNoFile}`;
+  dependencies: string[] | `${ArchiveDependencies}`;
   paths: {
     path: string;
-    on: "success" | "failure";
+    on: `${ArchiveOn}`;
   }[];
+}
+
+export enum ArchiveOn {
+  SUCCESS = "success",
+  FAILURE = "failure",
+}
+
+export enum IfNoFile {
+  WARN = "warn",
+  ERROR = "error",
+  IGNORE = "ignore",
+}
+
+export enum ArchiveDependencies {
+  ALL = "all",
+  NONE = "none",
 }
