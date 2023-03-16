bc0e00953a58349202170f8de6656c612f76db43
Shubh Bapna <sbapna@redhat.com>
2023-03-16 17:42:07 -0400

    added graph domain objects

---
+++ src/domain/graph.ts
@@ -0,0 +1,21 @@
+import { Dependency } from "@bc-cr/domain/dependencies";
+
+export type Graph = {
+  [vertex: string]: {
+    incoming: Set<string>
+    outgoing: Set<string>
+    dependency: Dependency
+  }
+}
+
+/**
+ * DFS visit flow: 
+ * none -> visting outgoing -> visited outgoing -> visiting incoming -> all
+ */
+export enum Visited {
+  VISITED_ALL,
+  VISITING_OUTGOING,
+  VISITED_OUTGOING,
+  VISITING_INCOMING,
+  VISITED_NONE
+}
